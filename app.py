a194d3a3eb08b6dbd8ff01849ac10e597483dc91
Javier Solis <javier.solis.guardia1@gmail.com>
2020-04-17 13:31:53 -0400

    commit 2020/04/17

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from time import time
 from random import random
 import json
+import flask_csv as cv
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'frida'
@@ -10,10 +12,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://javi:javiersolis12@loca
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
+
 class configuracion(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     tipo = db.Column(db.Integer)
     frec = db.Column(db.Integer)
+    potmax = db.Column(db.Float())
+    potmin = db.Column(db.Float())
+    tempmax = db.Column(db.Integer)
+    tempmin = db.Column(db.Integer)
+    checkbox = db.Column(db.String(15))
+    ip = db.Column(db.String(15))
+
 
 class tecnicos(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -21,6 +31,7 @@ class tecnicos(db.Model):
     apellidos = db.Column(db.String(80))
     email = db.Column(db.String(80))
     celular = db.Column(db.String(80))
+
 
 class todo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -30,73 +41,244 @@ class todo(db.Model):
     canal2 = db.Column(db.Float())
     canal3 = db.Column(db.Float())
     canal4 = db.Column(db.Float())
+    tempGabinete = db.Column(db.Float())
     hora = db.Column(db.Time())
     fecha = db.Column(db.Date())
 
+
 class ahora(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    temperatura = db.Column(db.Float(10))
-    humedad = db.Column(db.Float(10))
-    canal1 = db.Column(db.Float(10))
-    canal2 = db.Column(db.Float(10))
-    canal3 = db.Column(db.Float(10))
-    canal4 = db.Column(db.Float(10))
+    temperatura = db.Column(db.Float())
+    humedad = db.Column(db.Float())
+    canal1 = db.Column(db.Float())
+    canal2 = db.Column(db.Float())
+    canal3 = db.Column(db.Float())
+    canal4 = db.Column(db.Float())
+    tempGabinete = db.Column(db.Float())
     hora = db.Column(db.Time())
+
+class alarmas(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    codigo = db.Column(db.String(10))
+    descripcion = db.Column(db.String(100))
+    hora_inicial = db.Column(db.Time())
+    fec_inicial = db.Column(db.Date())
+    estado = db.Column(db.String(10))
+
+
+def menorQue(num1, num2):
+    if (num1 < num2):
+        return num1
+    if (num2 < num1):
+        return num2
+    if (num1 == num2):
+        return num1
+
+
+def mayorQue(num1, num2):
+    if (num1 > num2):
+        return num1
+    if (num2 > num1):
+        return num2
+    if (num1 == num2):
+        return num1
+
+
+def temperaturaMayorMenor():
+    Mayor = 0
+    Menor = 1000
+    temperaturas = todo.query.with_entities(todo.temperatura)
+    for temperatura in temperaturas:
+        aux = temperatura[0]
+        Mayor = mayorQue(Mayor, aux)
+        Menor = menorQue(Menor, aux)
+    return Mayor, Menor
+
+
+def ch1MayorMenor():
+    Mayor = 0
+    Menor = 1000
+    voltajes = todo.query.with_entities(todo.canal1)
+    for voltaje in voltajes:
+        aux = voltaje[0]
+        Mayor = mayorQue(Mayor, aux)
+        Menor = menorQue(Menor, aux)
+    return Mayor, Menor
+
+
+def ch2MayorMenor():
+    Mayor = 0
+    Menor = 1000
+    voltajes = todo.query.with_entities(todo.canal2)
+    for voltaje in voltajes:
+        aux = voltaje[0]
+        Mayor = mayorQue(Mayor, aux)
+        Menor = menorQue(Menor, aux)
+    return Mayor, Menor
+
+
+def HumedadMayorMenor():
+    Mayor = 0
+    Menor = 1000
+    humedades = todo.query.with_entities(todo.humedad)
+    for humedad in humedades:
+        aux = humedad[0]
+        Mayor = mayorQue(Mayor, aux)
+        Menor = menorQue(Menor, aux)
+    return Mayor, Menor
+
+
+def PromedioTemperatura():
+    suma = 0
+    aux = 0
+    temperaturas = todo.query.with_entities(todo.temperatura)
+    for temperatura in temperaturas:
+        suma = suma + temperatura[0]
+        aux += 1
+    promedio = suma/aux
+    return round(promedio, 3)
+
+
+def PromedioHumedad():
+    suma = 0
+    aux = 0
+    humedades = todo.query.with_entities(todo.humedad)
+    for humedad in humedades:
+        suma = suma + humedad[0]
+        aux += 1
+    promedio = suma/aux
+    return round(promedio, 3)
 
 
 @app.route('/')
 def home():
     sensores = ahora.query.filter_by(id=1).first()
-    return render_template('index.html', titulo='DASHBOARD TUTI', graph = sensores)
+    Alarmas = alarmas.query.filter_by(estado='activo').all()
+    return render_template('index.html', titulo='DASHBOARD TUTI', graph=sensores, notificaciones = len(Alarmas))
 
 
-@app.route('/vtecnicos')
-def VistaTecnicos():
-    return render_template('vtecnicos.html', titulo='Registro de Técnicos')
+@app.route('/configuracion')
+def Vista_config():
+    ConfiGlobal = configuracion.query.filter_by(id=1).first()
+    Alarmas = alarmas.query.filter_by(estado='activo').all()
+    return render_template('vconf.html', data=ConfiGlobal, titulo="Configuracion del Sistema", notificaciones = len(Alarmas))
 
 
-@app.route('/rtecnicos', methods=['POST'])
-def RegistroTecnicos():
-    if request.method == 'POST':
-        nuevoTecnico = tecnicos(
-            nombre = request.form['nombre'],
-            apellido = request.form['apellido'],
-            email = request.form['email'],
-            telf = request.form['celular'])
-        db.session.add(nuevoTecnico)
-        db.session.commit()
-        return redirect(url_for('VistaTecnicos'))
+@app.route('/ChangeConfig', methods=['POST'])
+def Actualizar_Config():
+    nuevaConfi = configuracion.query.filter_by(id=1).first()
+    nuevaConfi.tipo = request.form["antena"]
+    nuevaConfi.frec = request.form["frecuencia"]
+    nuevaConfi.potmax = request.form["potmax"]
+    nuevaConfi.potmin = request.form["potmin"]
+    nuevaConfi.tempmax = request.form["tempmax"]
+    nuevaConfi.tempmin = request.form["tempmin"]
+    if request.form.get('check'):
+        nuevaConfi.checkbox = "con CCM"
+        nuevaConfi.ip = request.form['ip']
+    else:
+        nuevaConfi.checkbox = "sin CCM"
+        nuevaConfi.ip = '0.0.0.0'
+    db.session.commit()
+    return redirect(url_for('Vista_config'))
 
-@app.route('/sensores')
-def prueba():
-    sensores = ahora.query.filter_by(id=1).first()
-    json_data = sensores
-    return jsonify({'temperatura' : sensores.temperatura,
-                    'humedad' : sensores.humedad,
-                    'canal1' : sensores.canal1,
-                    'canal2' : sensores.canal2,
-                    'canal3' : sensores.canal3,
-                    'canal4' : sensores.canal4})
 
 @app.route('/datos', methods=["GET", "POST"])
 def data1():
+    tmayor, tmenor = temperaturaMayorMenor()
+    ch1Mayor, ch1Menor = ch1MayorMenor()
+    ch2Mayor, ch2Menor = ch2MayorMenor()
+    humMayor, humMenor = HumedadMayorMenor()
+    PromTemp = PromedioTemperatura()
+    PromHum = PromedioHumedad()
     sensores = ahora.query.filter_by(id=1).first()
-    data = [(time() - 14400) * 1000, sensores.temperatura, sensores.humedad, round(sensores.canal1,4), round(sensores.canal2,4), round(sensores.canal3,4), round(sensores.canal4,4)]
+    Alarmas = alarmas.query.filter_by(estado='activo').all()
+    contador = 0
+    for alarma in Alarmas:
+        contador += 1
+
+    data = [(time() - 14400) * 1000,
+            sensores.temperatura,
+            sensores.humedad,
+            round(sensores.canal1, 4),
+            round(sensores.canal2, 4),
+            round(sensores.canal3, 4),
+            round(sensores.canal4, 4),
+            tmayor,
+            tmenor,
+            ch1Mayor,
+            ch1Menor,
+            ch2Mayor,
+            ch2Menor,
+            humMayor,
+            humMenor,
+            PromTemp,
+            PromHum,
+            sensores.tempGabinete,
+            contador]
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
     return response
 
-@app.route('/data', methods=["GET", "POST"])
-def data():
-    # Data Format
-    # [TIME, Temperature, Humidity]
-    Temperature = random() * 100
-    Humidity = random() * 55
-    data = [time() * 1000, Temperature, Humidity]
-    response = make_response(json.dumps(data))
-    response.content_type = 'application/json'
-    return response
 
+@app.route('/Vgraficos')
+def Vista_graficos():
+    Alarmas = alarmas.query.filter_by(estado='activo').all()
+    return render_template("vgraficos.html", titulo="Gráficos", notificaciones = len(Alarmas))
+
+@app.errorhandler(404)
+def not_found(e):
+    Alarmas = alarmas.query.filter_by(estado='activo').all()
+    return render_template("404.html", titulo="Error 404", notificaciones = len(Alarmas))
+
+@app.route('/datosJSON/<fec>/', methods=['GET', 'POST'])
+def ValoresJson(fec):
+    xData = []
+    data0 = []
+    data1 = []
+    data2 = []
+    SalidaDatos = {}
+    Datos = todo.query.filter_by(fecha=fec).all()
+    for dato in Datos:
+        aux = dato.hora
+        NumeroHoras = int(aux.hour) + (int(aux.minute) / 100)
+        xData.append(NumeroHoras)
+        data0.append(dato.canal1)
+        data1.append(dato.temperatura)
+        data2.append(dato.humedad)
+    SalidaDatos = {
+        "xData": xData,
+        "datasets": [{
+            "name": "Potencia Recepción",
+            "data": data0,
+            "unit": "[V]",
+            "type": "line",
+            "valueDecimals": 1
+        }, {
+            "name": "Temperatura",
+            "data": data1,
+            "unit": "°C",
+            "type": "line",
+            "valueDecimals": 0
+        }, {
+            "name": "Humedad",
+            "data": data2,
+            "unit": "%",
+            "type": "line",
+            "valueDecimals": 0
+        }]
+    }
+    return jsonify(SalidaDatos)
+
+@app.route('/alarmas')
+def MostrarAlarmas():
+    Alarmas = alarmas.query.filter_by(estado='activo').all()
+    return render_template('VistaAlarmas.html', notificaciones = len(Alarmas), datos = Alarmas, titulo="Alarmas")
+
+@app.route("/prueba")
+def index():
+    return cv.send_csv([{"id": 42, "foo": "bar"}, {"id": 91, "foo": "baz"}],
+                    "test.csv", ["id", "foo"])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- static/plugins/charts/prueba.js
+++ static/plugins/charts/prueba.js
@@ -30,15 +30,62 @@ var chartHumidity;
 
                 chartTemperatue.series[0].addPoint(data1, true, shiftTemperature);
                 chartHumidity.series[0].addPoint(data2, true, shiftHumidity);
-                $(".sensor1").text("");
-                $(".sensor1").text("Temperature : " + data1[1]);
+                
+                if (result[18] == 0){
+                    document.getElementById('alarmas1').innerHTML = '';
+                }
+                else{
+                    document.getElementById('alarmas1').innerHTML = result[18];
+                }
 
-                $(".sensor2").text("");
-                $(".sensor2").text("Humidity : " + data2[1],3);
+                //Cuadro 1x1
+                $(".TRtemperatura").text("");
+                $(".TRtemperatura").text("Temperatura: " + result[1] + "°C");
+                $(".TRhumedad").text("");
+                $(".TRhumedad").text("Humedad: " + result[2] + "%");
 
+                //Cuadro 1x2
+                $(".TRch1").text("");
+                $(".TRch1").text("Actual: " + result[3] + " [V]");
+                $(".TRch1max").text("");
+                $(".TRch1max").text("Máxima: " + result[9] + " [V]");
+                $(".TRch1min").text("");
+                $(".TRch1min").text("Mínima: " + result[10] + " [V]");
+
+                //Cuadro 1x3
+                $(".TRch2").text("");
+                $(".TRch2").text("Actual: " + result[4] + " [V]");
+                $(".TRch2max").text("");
+                $(".TRch2max").text("Máxima: " + result[11] + " [V]");
+                $(".TRch2min").text("");
+                $(".TRch2min").text("Mínima: " + result[12] + " [V]");
+
+                //Cuadro 2x1
+                $(".PromTemp").text("");
+                $(".PromTemp").text("Temperatura: " + result[15] + "°C");
+                $(".PromHum").text("");
+                $(".PromHum").text("Humedad: " + result[16] + "%");
+
+                //Cuadro 2x2
+                $(".Tmaxima").text("");
+                $(".Tmaxima").text("Temperatura Máxima: " + result[7] + "°C");
+                $(".Tminima").text("");
+                $(".Tminima").text("Temperatura Mínima: " + result[8] + "°C");
+                $(".Hmaxima").text("");
+                $(".Hmaxima").text("Humedad Máxima: " + result[13] + "%");
+                $(".Hminima").text("");
+                $(".Hminima").text("Humedad Mínima: " + result[14] + "%");
+
+                //Cuadro 3x3
+                $(".TempGabinete").text("");
+                $(".TempGabinete").text("Temperatura: " + result[17] + " °C");
+                $(".voltajeDC").text("");
+                $(".voltajeDC").text("Voltaje DC: " + result[5] + " [VDC]");
+                $(".voltajeAC").text("");
+                $(".voltajeAC").text("Voltaje AC: " + result[6] + " [VAC]");
+                
                 // call it again after one second
                 setTimeout(requestData, 5000);
-
             });
         }
 
@@ -55,7 +102,7 @@ var chartHumidity;
                 },
                 title:
                 {
-                    text: 'Temperature'
+                    text: 'Potencia de Recepcion Principal'
                 },
                 xAxis: {
                     type: 'datetime',
@@ -66,14 +113,14 @@ var chartHumidity;
                     minPadding: 0.2,
                     maxPadding: 0.2,
                     title: {
-                        text: 'Value',
+                        text: 'Voltaje',
                         margin: 80
                     }
                 },
                 series: [{
                     color: '#c23d23',
                     lineColor: '#303030',
-                    name: 'Temperature',
+                    name: 'Potencia Recibida',
                     data: []
                 }]
             });
@@ -90,7 +137,7 @@ var chartHumidity;
                 },
                 title:
                 {
-                    text: 'Humidity'
+                    text: 'Potencia de Recepcion Secundaria'
                 },
                 xAxis: {
                     type: 'datetime',
@@ -101,13 +148,13 @@ var chartHumidity;
                     minPadding: 0.2,
                     maxPadding: 0.2,
                     title: {
-                        text: 'Value',
+                        text: 'Voltaje',
                         margin: 80
                     }
                 },
                 series: [{
                     lineColor: '#1d82b8',
-                    name: 'Humidity',
+                    name: 'Potencia Recibida',
                     data: []
                 }]
             });
